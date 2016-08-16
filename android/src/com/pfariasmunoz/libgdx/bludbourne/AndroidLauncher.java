a3a94644aff580eab29807cf1359e38bf32591d1
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-16 11:10:35 -0300

    Initial commit

---
+++ android/src/com/pfariasmunoz/libgdx/bludbourne/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pfariasmunoz.libgdx.bludbourne;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pfariasmunoz.libgdx.bludbourne.BludBourne;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BludBourne(), config);
+	}
+}
