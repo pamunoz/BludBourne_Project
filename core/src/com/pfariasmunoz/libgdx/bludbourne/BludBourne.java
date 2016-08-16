4c5a8fc816b765dbde4f25bc9ebfe843a0fa732d
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-16 16:30:51 -0300

    add desktoplauncher config

--- core/src/com/pfariasmunoz/libgdx/bludbourne/BludBourne.java
+++ core/src/com/pfariasmunoz/libgdx/bludbourne/BludBourne.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
 public class BludBourne extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
---
+++ core/src/com/pfariasmunoz/libgdx/bludbourne/screens/MainGameScreen.java
@@ -0,0 +1,7 @@
+package com.pfariasmunoz.libgdx.bludbourne.screens;
+
+/**
+ * Created by Pablo Farias on 16-08-16.
+ */
+public class MainGameScreen {
+}
