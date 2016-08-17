8d2de48bbeddb6f7082ec53f541c02023013c62c
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-17 16:56:14 -0300

    add keyDown & keyUp to Entity

---
+++ core/src/com/pfariasmunoz/libgdx/bludbourne/PlayerController.java
@@ -0,0 +1,90 @@
+package com.pfariasmunoz.libgdx.bludbourne;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.math.Vector3;
+import com.sun.org.apache.bcel.internal.generic.IUSHR;
+
+/**
+ * Created by Pablo Farias on 17-08-16.
+ */
+public class PlayerController implements InputProcessor {
+    private final static String TAG = PlayerController.class.getSimpleName();
+
+    enum Keys {
+        LEFT, RIGHT, UP, DOWN, QUIT
+    }
+
+    enum Mouse {
+        SELECT, DOACTION
+    }
+
+    private static Map<Keys, Boolean> keys = new HashMap<PlayerController.Keys, Boolean>();
+    private static Map<Mouse, Boolean> mouseButton = new HashMap<PlayerController.Mouse, Boolean>();
+    private Vector3 lastMouseCoordinates;
+
+    // initialize the hashmap for inputs
+    static {
+        keys.put(Keys.LEFT, false);
+        keys.put(Keys.RIGHT, false);
+        keys.put(Keys.UP, false);
+        keys.put(Keys.DOWN, false);
+        keys.put(Keys.QUIT, false);
+    }
+
+    static {
+        mouseButton.put(Mouse.SELECT, false);
+        mouseButton.put(Mouse.DOACTION, false);
+    }
+
+    private Entity _player;
+
+    public PlayerController(Entity player) {
+        this.lastMouseCoordinates = new Vector3();
+        this._player = player;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Input.Keys.LEFT || keycode == Input.Keys.A) {
+            this.leftPressed();
+        }
+        if (keycode == Input.Keys.RIGHT || keycode == Input.Keys.D) {
+            this.rightPressed();
+        }
+        if (keycode == Input.Keys.UP || keycode == Input.Keys.W) {
+            this.upPressed();
+        }
+        if (keycode == Input.Keys.DOWN || keycode == Input.Keys.S) {
+            this.downPressed();
+        }
+        if (keycode == Input.Keys.Q) {
+            this.quitPressed();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if (keycode == Input.Keys.LEFT || keycode == Input.Keys.A) {
+            this.leftReleased();
+        }
+        if (keycode == Input.Keys.RIGHT || keycode == Input.Keys.D) {
+            this.rightReleased();
+        }
+        if (keycode == Input.Keys.UP || keycode == Input.Keys.W) {
+            this.upReleasrd();
+        }
+        if (keycode == Input.Keys.DOWN || keycode == Input.Keys.S) {
+            this.downReleased();
+        }
+        if (keycode == Input.Keys.Q) {
+            this.quitReleased();
+        }
+        return true;
+    }
+}
