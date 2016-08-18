729bb9bca2fe2c8480d3679fc48f144ebea230fa
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-18 14:29:52 -0300

    add Entity constructor

--- core/src/com/pfariasmunoz/libgdx/bludbourne/Entity.java
+++ core/src/com/pfariasmunoz/libgdx/bludbourne/Entity.java
@@ -1,13 +1,20 @@
 package com.pfariasmunoz.libgdx.bludbourne;
 
-import java.util.UUID;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
+import java.util.ArrayList;
+
+import java.util.UUID;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
+
 import com.badlogic.gdx.utils.Array;
 /**
  * Created by Pablo Farias on 17-08-16.
@@ -15,45 +22,89 @@ import com.badlogic.gdx.utils.Array;
 public class Entity {
 
     private static final String TAG = Entity.class.getSimpleName();
-    private static final String _defaultSpritePath = "sprites/characters/Warrior.png";
+    private Json _json;
+    private EntityConfig _entityConfig;
 
-    private Vector2 _velocity;
-    private String _entityID;
 
-    private Direction _currentDirection = Direction.LEFT;
-    private Direction _previousDirection = Direction.UP;
 
-    private Animation _walkLeftAnimation;
-    private Animation _walkRightAnimation;
-    private Animation _walkUpAnimation;
-    private Animation _walkDownAnimation;
+    public static enum Direction {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT;
 
-    private Array<TextureRegion> _walkLeftFrames;
-    private Array<TextureRegion> _walkRightFrames;
-    private Array<TextureRegion> _walkUpFrames;
-    private Array<TextureRegion> _walkDownFrames;
-
-    protected Vector2 _nextPlayerPosition;
-    protected Vector2 _currentPlayerPosition;
-    protected State _state = State.IDLE;
-    protected float _frameTime = 0f;
-    protected Sprite _frameSprite = null;
-    protected TextureRegion _currentFrame = null;
-
-    public final int FRAME_WIDTH = 16;
-    public final int FRAME_HEIGHT = 16;
-    public static Rectangle boundingBox;
-
-    public enum State {
-        IDLE, WALKING
+        static public Direction getRandomNext() {
+            return Direction.values()[MathUtils.random(Direction.values().length - 1)];
+        }
+        public Direction getOpposite() {
+            if (this == LEFT) {
+                return RIGHT;
+            } else if (this == RIGHT) {
+                return LEFT;
+            } else if (this == UP) {
+                return DOWN;
+            } else {
+                return UP;
+            }
+        }
     }
 
-    public enum Direction {
-        UP, RIGHT, DOWN, LEFT
+    public static enum State {
+        IDLE,
+        WALKING,
+        IMMOBILE; // This should always be last
+
+        static public State getRandomNext() {
+            // Ignore IMMOBILE which should be last state
+            return State.values()[MathUtils.random(State.values().length - 2)];
+        }
     }
 
-    public Entity() {
-        initEntity();
+    public static enum AnimationType {
+        WALK_LEFT,
+        WALK_RIGHT,
+        WALK_UP,
+        WALK_DOWN,
+        IDLE,
+        IMMOBILE
+    }
+
+    public static final int FRAME_WIDTH = 16;
+    public static final int FRAME_HEIGHT = 16;
+
+    private static final int MAX_COMPONENTS = 5;
+    private Array<Component> _components;
+    private InputComponent _inputComponent;
+    private GraphicsComponent _graphicsComponent;
+    private PhysicsComponent _physicsComponent;
+
+    public Entity(InputComponent inputComponent, PhysicsComponent physicsComponent, GraphicsComponent graphicsComponent) {
+        _entityConfig = new EntityConfig();
+        _json = new Json();
+
+        _components = new Array<Component>(MAX_COMPONENTS);
+
+        _inputComponent = inputComponent;
+        _physicsComponent = physicsComponent;
+        _graphicsComponent = graphicsComponent;
+        _components.add(_inputComponent);
+        _components.add(_physicsComponent);
+        _components.add(_graphicsComponent);
+    }
+
+    public EntityConfig getEntityConfig() {
+        return _entityConfig;
+    }
+
+    public void sendMessage(Component.MESSAGE messageType, String ... args) {
+        String fullMessage = messageType.toString();
+        for (String string : args) {
+            fullMessage += Component.MESSAGE_TOKEN + string;
+        }
+
+        for (Component component : _components) {
+            component.receiveMessage(fullMessage);
+        }
     }
 
     public void initEntity() {
---
+++ core/src/com/pfariasmunoz/libgdx/bludbourne/EntityConfig.java
@@ -0,0 +1,13 @@
+package com.pfariasmunoz.libgdx.bludbourne;
+
+import com.badlogic.gdx.math.GridPoint2;
+import com.badlogic.gdx.utils.Array;
+import com.pfariasmunoz.libgdx.bludbourne.Entity.AnimationType;
+/**
+ * Created by Pablo Farias on 18-08-16.
+ */
+public class EntityConfig {
+    Array<AnimationConfig> animationConfig;
+    Entity.State state
+
+}
